d96607773fb4a994f011f34808be192929922596
Lourenço <48692097+ldcmulling@users.noreply.github.com>
2019-03-26 14:55:05 -0300

    Update main.c

--- AGENDA/main.c
+++ AGENDA/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,6 +7,19 @@ char nome[20];
 int idade, telefone;
 };
 
+void adicionar(int *c,struct pessoa *pet){
+	for(*c=1;*c!=0;){
+		printf (" Digite 1 para adicionar \ Digite 0 para sair \n");
+		scanf("%d",c);
+		printf("Digite o Nome \n");
+		scanf(" %s ", pet->nome);
+		printf("Digite a idade\n");
+		scanf(" %d ", pet->idade);	
+		printf("Digite o Telefone\n");
+		scanf(" %d ", pet->telefone);
+		
+	}
+}
 
 
 
@@ -14,10 +28,20 @@ int main(int argc, char **argv){
     int *cp, *c;
     float *a;
     struct pessoa *pet;
-
-    buffer=malloc(2*sizeof(int)+sizeof(float)+sizeof(struct pessoa)**cp);
-
-
+    
+	printf("debug\n");
+	
+	printf("debug\n");
+    buffer=malloc(2*sizeof(int)+sizeof(float)+sizeof(struct pessoa));
+	cp=buffer;
+	c=cp+1;
+	a= (float*)c+1;
+	pet=(struct pessoa*)a+1;
+	*cp=0;
+	
+	adicionar(c,pet);
+	
+	printf("debug\n");
 
 	return 0;
 }
