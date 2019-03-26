bc9f78ba2a31738fe1a7834219b7a452f74f5872
Lourenço <48692097+ldcmulling@users.noreply.github.com>
2019-03-26 13:44:12 -0300

    Update main.c

--- AGENDA/main.c
+++ AGENDA/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct pessoa{
+char nome[20];
+int idade, telefone;
+};
+
+
+
+
+int main(int argc, char **argv){
+    void *buffer;
+    int *cp, *c;
+    float *a;
+    struct pessoa *pet;
+
+    buffer=malloc(2*sizeof(int)+sizeof(float)+sizeof(struct pessoa)**cp);
+
+
+
+	return 0;
+}
